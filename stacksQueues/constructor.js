9522de722660613191a45cbd3c042f58f230fcd3
Kevin <kevinma91@yahoo.com>
2017-09-16 17:53:23 -0800

    updated

--- stacksQueues/constructor.js
+++ stacksQueues/constructor.js
@@ -34,70 +34,71 @@ function LinkedStackOfStrings(){
 // EX 2 // Stacks w/ ARRAY OF STRINGS
 // Constructors
 
-// function ArrayStack(){
-//   this.array = [];
-//   this.size = 0;
-//
-//   this.isEmpty = function(){
-//     return this.size == 0;
-//   }
-//
-//   this.push = function(item){
-//     this.array[this.size++] = item;
-//   }
-//
-//   this.pop = function(){
-//     var item = this.array[--this.size];
-//     this.array[this.size] = null;
-//     return item;
-//   }
-//
-//   this.peek = function(){
-//     return this.array[--this.size];
-//   }
-// }
-//
-// // Initialize ArrayStack and implement methods
+function ArrayStack(){
+  this.array = [];
+  this.size = 0;
+
+  this.isEmpty = function(){
+    return this.size == 0;
+  }
+
+  this.push = function(item){
+    this.array[this.size++] = item;
+  }
+
+  this.pop = function(){
+    var item = this.array[--this.size];
+    this.array[this.size] = null;
+    return item;
+  }
+
+  this.peek = function(){
+    return this.array[--this.size];
+  }
+}
+
+// Initialize ArrayStack and implement methods
 // var newone = new ArrayStack();
 // console.log(newone.isEmpty());
 // newone.push(5);
 // console.log(newone.peek());
 // newone.pop();
 
-// // EX 3 // Stacks w/ KEY VALUE PAIRS
-// // Constructors
-// function stack(){
-//   this.count = 0;
-//   this.storage = {};
-//
-//   // Adds value at end of stack
-//   this.push = function(val){
-//     this.storage[this.count] = val;
-//     this.count++;
-//   }
-//
-//   // Removes and returns value at end of stack
-//   this.pop = function(){
-//     if(this.count === 0){
-//       return undefined;
-//     }
-//
-//     this.count--;
-//     var result = this.storage[this.count];
-//     delete this.storage[this.count];
-//     return result;
-//   }
-//
-//   this.size = function(){
-//     return this.count;
-//   }
-//
-//   // Returns value at end of stack WITHOUT REMOVING ITEM
-//   this.peek = function(){
-//     return this.storage[this.count-1];
-//   }
-// }
-//
+// EX 3 // Stacks w/ KEY VALUE PAIRS
+// Constructors
+function stack(){
+  this.count = 0;
+  this.storage = {};
+
+  // Adds value at end of stack
+  this.push = function(val){
+    this.storage[this.count] = val;
+    this.count++;
+  }
+
+  // Removes and returns value at end of stack
+  this.pop = function(){
+    if(this.count === 0){
+      return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  this.size = function(){
+    return this.count;
+  }
+
+  // Returns value at end of stack WITHOUT REMOVING ITEM
+  this.peek = function(){
+    return this.storage[this.count-1];
+  }
+}
+
+// Initialize ArrayStack and implement methods
 // var mystack = new stack()
 // mystack.push(3);
 // mystack.push(5);
